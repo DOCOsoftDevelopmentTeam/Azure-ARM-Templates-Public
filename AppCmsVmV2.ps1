d1e4f58c1c776c4b7cd78fb60d1179ac18151eec
DOCOsoftDevelopmentTeam <61588112+DOCOsoftDevelopmentTeam@users.noreply.github.com>
2020-03-03 10:19:48 +0000

    Update AppCmsVmV2.ps1

--- AppCmsVmV2.ps1
+++ AppCmsVmV2.ps1
@@ -8,3 +8,11 @@ msiexec /i "https://github.com/DOCOsoftDevelopmentTeam/Azure-ARM-Templates-Publi
 # Install Microsoft® SQL Server® 2014 Feature Pack
 # Source : https://www.microsoft.com/en-ie/download/details.aspx?id=42295
 msiexec /i "https://github.com/DOCOsoftDevelopmentTeam/Azure-ARM-Templates-Public/raw/master/SharedManagementObjects.msi" /qn /norestart
+
+# Install Microsoft® SQL Server® 2012 Native Client - QFE
+# Source : https://www.microsoft.com/en-us/download/details.aspx?id=50402
+msiexec /i "https://github.com/DOCOsoftDevelopmentTeam/Azure-ARM-Templates-Public/raw/master/sqlncli.msi" /qn /norestart
+
+# Install Microsoft Core XML Services (MSXML) 6.0
+# Source : https://www.microsoft.com/en-ie/download/details.aspx?id=3988
+msiexec /i "https://github.com/DOCOsoftDevelopmentTeam/Azure-ARM-Templates-Public/raw/master/msxml6_x64.msi" /qn /norestart
