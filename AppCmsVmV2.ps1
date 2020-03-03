ecc56cc4c1cb8a90b9859ce8c7a7185d4364562e
DOCOsoftDevelopmentTeam <61588112+DOCOsoftDevelopmentTeam@users.noreply.github.com>
2020-03-03 09:37:01 +0000

    1. Adding SQL CLR Types

--- AppCmsVmV2.ps1
+++ AppCmsVmV2.ps1
@@ -1,1 +1,5 @@
-﻿Install-WindowsFeature -name Web-Server -IncludeManagementTools
+# Install IIS
+Install-WindowsFeature -name Web-Server -IncludeManagementTools
+
+# Install SQL CLR Types
+msiexec /i "https://github.com/DOCOsoftDevelopmentTeam/Azure-ARM-Templates-Public/raw/master/SQLSysClrTypes.msi" /qn /norestart
