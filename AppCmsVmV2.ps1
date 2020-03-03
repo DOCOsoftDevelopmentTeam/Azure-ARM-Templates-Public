8f7a8c84d875c2221d2c846b7f66adbc8648bc06
DOCOsoftDevelopmentTeam <61588112+DOCOsoftDevelopmentTeam@users.noreply.github.com>
2020-03-03 10:09:45 +0000

    1. Adding Microsoft® SQL Server® 2014 Feature Pack

--- AppCmsVmV2.ps1
+++ AppCmsVmV2.ps1
@@ -1,5 +1,10 @@
 # Install IIS
 Install-WindowsFeature -name Web-Server -IncludeManagementTools
 
-# Install SQL CLR Types
+# Install Microsoft® SQL Server® CLR Types
+# Source : https://www.microsoft.com/en-ie/download/details.aspx?id=42295
 msiexec /i "https://github.com/DOCOsoftDevelopmentTeam/Azure-ARM-Templates-Public/raw/master/SQLSysClrTypes.msi" /qn /norestart
+
+# Install Microsoft® SQL Server® 2014 Feature Pack
+# Source : https://www.microsoft.com/en-ie/download/details.aspx?id=42295
+msiexec /i "https://github.com/DOCOsoftDevelopmentTeam/Azure-ARM-Templates-Public/raw/master/SharedManagementObjects.msi" /qn /norestart
